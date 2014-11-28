f6ec68c0ea2cd374df8227bc2d7b95011d8b6571
mercuron <mercuron@gmx.ch>
2014-11-28 08:58:11 +0100

    housekeeping

---
+++ src/test/java/ch/fhnw/i4ds/helio/coordinate/converter/Hcc2HpcConverterTest.java
@@ -0,0 +1,36 @@
+package ch.fhnw.i4ds.helio.coordinate.converter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.fhnw.i4ds.helio.coordinate.converter.Hcc2HpcConverter.ConversionOptions;
+import ch.fhnw.i4ds.helio.coordinate.coord.HeliocentricCartesianCoordinate;
+import ch.fhnw.i4ds.helio.coordinate.coord.HelioprojectiveCartesianCoordinate;
+import ch.fhnw.i4ds.helio.coordinate.util.Constants;
+
+public class Hcc2HpcConverterTest {
+	private Hcc2HpcConverter converter;
+	
+	@Before
+	public void setup() {
+		converter = new Hcc2HpcConverter();
+	}
+    
+    /**
+     * Adapted from Sunpy test_wcs.py.
+     */
+    @Test
+    public void convert_custom_l0_b0() {
+    	ConversionOptions opt = Hcc2HpcConverter.newConversionOptions();
+    	HeliocentricCartesianCoordinate hcc = new HeliocentricCartesianCoordinate(28748691, 22998953);
+    	
+    	HelioprojectiveCartesianCoordinate hpc = converter.convert(hcc, opt);
+    	System.out.println(hcc);
+    	System.out.println(hpc);
+    	
+    	Assert.assertEquals(40.0, hpc.getX(), 1.0);
+    	Assert.assertEquals(32.0, hpc.getY(), 1.0);
+    	Assert.assertEquals(Constants.AU.getValue(), hpc.getSunDistance(), 1.0);
+    }
+}
